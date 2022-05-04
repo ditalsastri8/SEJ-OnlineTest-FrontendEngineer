69c0d8f0894919a5881b7cf071eea1932556a1cf
ditalsastri8 <60680401+ditalsastri8@users.noreply.github.com>
2022-05-04 00:21:24 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <h1>Hello, Sejutacita For You!</h1>
+}
+export default App;
